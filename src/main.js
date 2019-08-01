71708da7ca5f16a06bede8af48aa03f837248d97
zhangxiaodong <zhangxiaodong@forwardx.com>
2019-08-01 09:15:13 +0800

    删除packagelock,增加注释

--- src/main.js
+++ src/main.js
@@ -4,8 +4,13 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// 本地跑，测试插件可用性
 import myPlugin from './lib/keyboard'
 Vue.use(myPlugin);
+
+// npm包 keyboard 文件夹放在 node_modules 中测试包可用性
+// import keyboard from 'zxd-pay-keyboard'
+// Vue.use(keyboard);
 
 Vue.config.productionTip = false
 
